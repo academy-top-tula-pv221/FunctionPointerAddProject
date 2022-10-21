101de25fa6922094ec7ad91dc7820d8504c6e61b
GitStudent2022 <torelgame@mail.ru>
2022-10-21 21:17:48 +0300

    Добавьте файлы проекта.

---
+++ FunctionPointerAddProject/FunctionPointerAddProject.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+int Sum(int a, int b)
+{
+    return a + b;
+}
+
+int Mult(int a, int b)
+{
+    return a * b;
+}
+
+int(*funcPtr)(int, int);
+int(*SelectOperation(char sign))(int, int)
+{
+    switch (sign)
+    {
+    case '+': return Sum;
+    case '*': return Mult;
+    case '-': return [](auto a, auto b) { return a - b; };
+    default:
+        return nullptr;
+    }
+}
+
+int main()
+{
+    int(*funcPtr)(int, int);
+    funcPtr = SelectOperation('+');
+}
